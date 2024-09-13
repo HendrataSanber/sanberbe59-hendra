fa243e3bb00ee5e77c92aeb6f436c7c7ff2b9a37
hendrata.loekman@gmail.com <hendrata.loekman@gmail.com>
2024-09-13 20:51:16 +0700

    Update 2

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-"# sanberbe59-hendra" 
+# Hendra's Node.js Final Project
+
+## Deployment Details
+
+The project is deployed on Vercel at [https://sanberbe59-hendra.vercel.app](https://sanberbe59-hendra.vercel.app).
+
+## API Endpoints
+
+* `POST /api/auth/login`: Login to app
+* `POST /api/auth/register`: Register to app
+* `POST /api/auth/me`: Get user detail
+* `PUT /api/auth/update-profile`: Update profile
+
+* `GET /api/categories`: Get all categories
+* `POST /api/categories`: Create new category
+* `GET /api/categories/{id}`: Get one category based on id
+* `PUT /api/categories/{id}`: Update one category
+* `DELETE /api/categories/{id}`: Delete one category
+
+* `GET /api/products`: Get all products
+* `POST /api/products`: Create new product
+* `GET /api/products/{id}`: Get one product based on id
+* `PUT /api/products/{id}`: Update one product
+* `DELETE /api/products/{id}`: Delete one product
+
+* `POST /api/upload`: Upload one image
+* `POST /api/uploads`: Upload multiple image
+
+* `POST /api/orders`: Create new order
+* `GET /api/orders`: Get all orders
+* `GET /api/orders/{id}`: Get one order based on id
+
+## Swagger Documentation
+
+The Swagger documentation is available at [https://sanberbe59-hendra.vercel.app/docs](https://sanberbe59-hendra.vercel.app/docs).
--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -2,59 +2,59 @@ import swaggerAutogen from "swagger-autogen";
 
 
 const doc = {
-    info: {
-      version: "v0.0.1",
-      title: "Dokumentasi API Hendra Co Ltd",
-      description: "Dokumentasi API Hendra Co Ltd",
+  info: {
+    version: "v0.0.1",
+    title: "Dokumentasi API Hendra Co Ltd",
+    description: "Dokumentasi API Hendra Co Ltd",
+  },
+  servers: [
+    {
+      url: "https://sanber-vercel.vercel.app/api",
+      description: "Local Server",
     },
-    servers: [
-      {
-        url: "http://localhost:3000/api",
-        description: "Local Server",
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
       },
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "http",
-          scheme: "bearer",
-        },
+    },
+    schemas: {
+      LoginRequest: {
+        email: "joni2024@yopmail.com",
+        password: "123412341",
       },
-      schemas: {
-        LoginRequest: {
-          email: "joni2024@yopmail.com",
-          password: "123412341",
-        },
-        RegisterRequest: {
-          fullName: "joni joni",
-          username: "joni2024",
-          email: "joni2024@yopmail.com",
-          password: "123412341",
-          confirmPassword: "123412341",
-        },
-        UpdateProfileRequest: {
-          fullName: "joni joni",
-          username: "joni2024",
-          email: "joni2024@yopmail.com",
-          password: "123412341",
-          confirmPassword: "123412341",
-        },
-        OrderCreateRequest:{
-          "grandTotal": 15000,
-          "orderItems": [
-            {
+      RegisterRequest: {
+        fullName: "joni joni",
+        username: "joni2024",
+        email: "joni2024@yopmail.com",
+        password: "123412341",
+        confirmPassword: "123412341",
+      },
+      UpdateProfileRequest: {
+        fullName: "joni joni",
+        username: "joni2024",
+        email: "joni2024@yopmail.com",
+        password: "123412341",
+        confirmPassword: "123412341",
+      },
+      OrderCreateRequest: {
+        "grandTotal": 15000,
+        "orderItems": [
+          {
             "name": "Kemeja kadita",
             "productId": "66d2d707352121e4ecdf19b9",
             "price": 15000,
             "quantity": 1
-            }
-          ]
-        }
-      },
+          }
+        ]
+      }
     },
-  };
+  },
+};
 
-  
+
 const outputFile = "./swagger_output.json";
 const endpointsFiles = ["../routes/api.ts"];
 
